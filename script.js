d2e75a149c4be014854385434907520c02a164a8
iurinhg <iurinhg2@gmail.com>
2022-08-08 20:06:04 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href;
+console.log(url);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const elementoAtivo = document.querySelector('.ativo').innerHTML;
+console.log(elementoAtivo);
+
+
+// Retorne a linguagem do navegador
+const linguagem = window.navigator.language;
+console.log(linguagem);
+
+// Retorne a largura da janela
+const largura = window.innerWidth;
+console.log(largura);
+
